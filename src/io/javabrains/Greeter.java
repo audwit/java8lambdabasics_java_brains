3d0025138caed7cc294e53e029c0962a4cfd0d69
Kaykobad <kayes.kaykobad@marquette.edu>
2019-05-10 13:20:47 -0500

    Java 8 Lambda Basics 9 - Lambdas vs Interface Implementations

--- src/io/javabrains/Greeter.java
+++ src/io/javabrains/Greeter.java
@@ -10,10 +10,36 @@ public class Greeter
 	
 	public static void main(String[] args) 
 	{
+			// Greeting the old fashioned way with an interface
 			Greeter greeter = new Greeter();
 			HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 			greeter.greet(helloWorldGreeting);
+			// Greeting the old fashioned way with an interface
+			
+			// Greeting from lambda function. This can be though as a shortcut to the anonymous inner class approach below. But lambda expressions are not exactly like anonymous inner class which we will see in future tutorials. 
+			Greeting myLambdaGreetingFunction = () -> System.out.println("Hello world from lambda");
+			myLambdaGreetingFunction.perform();
+			// Greeting from lambda function
+			
+			// Greeting from anonymous class
+			Greeting anonymousInnerClassGreeting = new Greeting()
+			{
+				
+				@Override
+				public void perform()
+				{
+					System.out.println("Hello world from Anonymous Inner Class Greeting");
+					
+				}
+			};
+			
+			anonymousInnerClassGreeting.perform();
+			// Greeting from anonymous class
+			
+			greeter.greet(myLambdaGreetingFunction);
+			greeter.greet(anonymousInnerClassGreeting);
 
 	}
 
 }
+
